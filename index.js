f5ab33a342b7f2e1563d8d2026a480f2d29b314a
Joe Dietrich <joe@joedietrich.net>
2021-06-28 22:16:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-
-// Write your code here
+const num1 = 2, num2 = 31, num3 = 24, num4 = 20;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 100 + 1);
+const mod = num3 % num4;
+const max = Math.max(1, -5, 18, -Infinity, 20);
